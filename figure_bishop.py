b442e8e1ab6726a1f959c8da7c310802c96e663e
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-24 18:37:54 +0100

    init figure abstract class and 6 class for every figure

---
+++ figure_bishop.py
@@ -0,0 +1,14 @@
+from figure import Figure
+
+
+class Bishop(Figure):
+
+    def __init__(self, current_field: str):
+        super().__init__(current_field)
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self, dest_field: str) -> bool:
+        pass
+
---
+++ figure_queen.py
@@ -0,0 +1,13 @@
+from figure import Figure
+
+
+class Queen(Figure):
+
+    def __init__(self, current_field: str):
+        super().__init__(current_field)
+
+    def list_available_moves(self):
+        pass
+
+    def validate_move(self, dest_field: str) -> bool:
+        pass
