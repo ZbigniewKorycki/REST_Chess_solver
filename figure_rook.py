4b45f6fe89fe1ca5fa972c24de382a94bda45abb
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-24 21:55:33 +0100

    add functions to rook for listing available moves and validate move

--- figure_rook.py
+++ figure_rook.py
@@ -1,13 +1,38 @@
 from figure import Figure
-
+from chessboard import Chessboard
 
 class Rook(Figure):
 
     def __init__(self, current_field: str):
         super().__init__(current_field)
+        self.chessboard = Chessboard()
+        self.available_moves = self.list_available_moves()
 
     def list_available_moves(self):
-        pass
+        available_moves = []
+        if self.current_field in self.chessboard.fields:
+            directions = [
+                Chessboard.get_field_after_moving_up,
+                Chessboard.get_field_after_moving_down,
+                Chessboard.get_field_after_moving_left,
+                Chessboard.get_field_after_moving_right
+            ]
+            for direction in directions:
+                for distance in range(1, 8):
+                    new_field = direction(self.current_field, distance)
+                    if new_field in self.chessboard.fields:
+                        available_moves.append(new_field)
+                    else:
+                        break
+        else:
+            return []
+        return sorted(available_moves)
 
     def validate_move(self, dest_field: str) -> bool:
-        pass
+        if self.chessboard.check_if_field_in_chessboard(dest_field):
+            if dest_field.upper() in self.available_moves:
+                return True
+            else:
+                return "Field not available."
+        else:
+            return "Field does not exists."
