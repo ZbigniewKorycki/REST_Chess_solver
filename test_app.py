7d28bc783e04cee1d276440ecd4788ac76858e51
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-26 18:49:42 +0100

    add tests app pawn for 6,7,8 and valid king

--- test_app.py
+++ test_app.py
@@ -28,14 +28,7 @@ def test_get_list_available_moves_invalid_field(client):
 def test_get_list_available_moves_valid_knight(client):
     response = client.get("/api/v1/knight/d4")
     data = response.json
-    assert "B3" in data["availableMoves"]
-    assert "B5" in data["availableMoves"]
-    assert "C2" in data["availableMoves"]
-    assert "C6" in data["availableMoves"]
-    assert "E2" in data["availableMoves"]
-    assert "E6" in data["availableMoves"]
-    assert "F3" in data["availableMoves"]
-    assert "F5" in data["availableMoves"]
+    assert "B3" and "B5" and "C2" and "C6" and "E2" and "E6" and "F3" and "F5" in data["availableMoves"]
     assert 8 == len(data["availableMoves"])
     assert "knight" in data["figure"]
     assert response.status_code == 200
@@ -55,12 +48,53 @@ def test_get_list_available_moves_valid_pawn_second_row(client):
     response = client.get("/api/v1/pawn/a2")
     data = response.json
     assert "A1" in data["availableMoves"]["forBlacks"]
-    assert "A3" in data["availableMoves"]["forWhites"]
-    assert "A4" in data["availableMoves"]["forWhites"]
+    assert "A3" and "A4" in data["availableMoves"]["forWhites"]
     assert None is data["error"]["forWhites"]
     assert None is data["error"]["forBlacks"]
     assert "pawn" in data["figure"]
     assert response.status_code == 200
 
 
+def test_get_list_available_moves_valid_pawn_sixth_row(client):
+    response = client.get("/api/v1/pawn/g6")
+    data = response.json
+    assert "G5" in data["availableMoves"]["forBlacks"]
+    assert 1 == len(data["availableMoves"]["forBlacks"])
+    assert "G7" in data["availableMoves"]["forWhites"]
+    assert 1 == len(data["availableMoves"]["forWhites"])
+    assert None is data["error"]["forWhites"]
+    assert None is data["error"]["forBlacks"]
+    assert response.status_code == 200
+
+
+def test_get_list_available_moves_valid_pawn_seventh_row(client):
+    response = client.get("/api/v1/pawn/b7")
+    data = response.json
+    assert "B6" and "B5" in data["availableMoves"]["forBlacks"]
+    assert 2 == len(data["availableMoves"]["forBlacks"])
+    assert "B8" in data["availableMoves"]["forWhites"]
+    assert 1 == len(data["availableMoves"]["forWhites"])
+    assert None is data["error"]["forWhites"]
+    assert None is data["error"]["forBlacks"]
+    assert response.status_code == 200
+
+
+def test_get_list_available_moves_valid_pawn_eighth_row(client):
+    response = client.get("/api/v1/pawn/c8")
+    data = response.json
+    assert [] == data["availableMoves"]["forBlacks"]
+    assert [] == data["availableMoves"]["forWhites"]
+    assert None is data["error"]["forWhites"]
+    assert "invalid field for figure" in data["error"]["forBlacks"]
+    assert response.status_code == 200
+
+
+def test_get_list_available_moves_valid_king(client):
+    response = client.get("/api/v1/king/b1")
+    data = response.json
+    assert "A1" and "A2" and "B2" and "C1" and "C2" in data["availableMoves"]
+    assert 5 == len(data["availableMoves"])
+    assert None is data["error"]
+    assert "king" in data["figure"]
+    assert response.status_code == 200
 
