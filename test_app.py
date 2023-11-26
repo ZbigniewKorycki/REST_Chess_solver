c4b3eda070d95f4852b47fa68b12ba2416322a72
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-26 18:21:22 +0100

    add tests getting invalid figure and field for list available moves

--- test_app.py
+++ test_app.py
@@ -7,3 +7,19 @@ def client():
     with app.test_client() as client:
         yield client
 
+
+def test_get_list_available_moves_invalid_figure(client):
+    response = client.get("/api/v1/non_existing_figure/a4")
+    data = response.json
+    assert "invalid figure" in data["error"]
+    assert "a4" in data["currentField"]
+    assert [] == data["availableMoves"]
+    assert response.status_code == 404
+
+
+def test_get_list_available_moves_invalid_field(client):
+    response = client.get("/api/v1/pawn/a9")
+    data = response.json
+    assert "current field does not exist" in data["error"]
+    assert [] == data["availableMoves"]
+    assert response.status_code == 409
